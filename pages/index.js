183110310da2e42c87bfd670008fc8401936c4f7
Tim Starbuck <timstarbuck@gmail.com>
2021-03-14 14:46:17 -0500

    initial work

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,142 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Main.module.css'
 
 export default function Home() {
+  const [ipsumDat, setIpsumDat] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [values, setValues] = useState({count: 5, includeQuotes: true});
+  const [paragraphs, setParagraphs] = useState([]);
+  const handleInputChange = e => {
+    const {name} = e.target;
+    const data = e.target.type == "checkbox" ? e.target.checked : e.target.value;
+    setValues({...values, [name]: data});
+  }
+  const getRandomInt = (max) => {
+    return (Math.floor(Math.random() * Math.floor(max)));
+  }
+  const generate = e => {
+    console.log(values);
+    // 4 sentences per paragraph? random 3 - 5?
+    // how many words per sentence? random 5 - 10?
+    let paragraphs = [];
+    for (let i = 0; i < values.count; i++) {
+      let paragraph = "";
+      const sentences = getRandomInt(10);
+      sentences = sentences < 3 ? 3: sentences;
+      
+      console.log('Sentences ' + sentences);
+      
+      
+      for (let s = 0; s < sentences; s++) {
+        if (values.includeQuotes && getRandomInt(10) < 3) {
+           paragraph += ipsumDat.quotes[getRandomInt(ipsumDat.quotes.length)];
+           paragraph += " ";
+        } else {
+          let sentence = "";
+          let words = getRandomInt(10);
+          words = words < 5 ? 5 : words;
+          console.log("Words: " + words);
+          for(let w = 0; w < words; w++){
+            sentence += ipsumDat.words[getRandomInt(ipsumDat.words.length)]
+            sentence += " ";
+          }
+          sentence = sentence.replace(/\s+$/, '') + '. ';
+          paragraph += sentence;
+        }
+      }
+      
+      
+      paragraphs[i] = paragraph;
+    }
+    setParagraphs(paragraphs);
+    console.log(paragraphs);
+  }
+  
+  
+
+  useEffect(() => {
+    fetch("/ipsum.json")
+    .then(res =>  res.json())
+    .then(jsonData =>  setIpsumDat(jsonData))
+    .catch(err => {
+      console.log(err);
+      setHasError(true)
+    })
+  } , [])
+
+  console.log(ipsumDat);
+  if (hasError) {
+    return <div>
+      Oh Snap! Something unexpected happened. Please try again;
+    </div>
+  }
+  
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Tesla Ipsum</title>
+        <meta name="description" content="Tesla focused Lorem Ipsum Generator. Tesla Ipsum filler."/>
+        </Head>
+      <div className={styles.content}>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <header className={styles.header} role="img" aria-label="Tesla Model 3 Cars"> 
+        <h1><a href="#" id="logo">Tesla Ipsum</a></h1>
+      </header>
+      
+      <div className={styles.main}>
+        
+        <article>
+          <div className={styles.formContainer}>
+            <div >
+              Paragraphs
+            </div>
+            <div >
+              <input type="number" min="1" max="10" value="5" 
+                    name="count"
+                    onChange={handleInputChange}
+                    value={values.count} />
+            </div>
+   
+            <div >
+              Musk Quotes?
+            </div>
+            <div >
+              <input type="checkbox" name="includeQuotes"
+                    onChange={handleInputChange}
+                    checked={values.includeQuotes} />
+            </div>
+            <div></div>
+            <div >
+              <button className="button small" onClick={generate}>Generate</button>
+            </div>      
+          </div>
+     
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          {paragraphs.length > 0 &&
+          <div>
+            {paragraphs.map( (p, i) => {
+              return <p key={i}>{p}</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            })}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
+          }  
+        </article>
+        
+        <nav className={styles.nav}></nav>
+        <aside className={styles.aside}></aside>
+      </div>
+      </div>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <div>
+          Starguy Software © {new Date().getFullYear()}
+        </div>
       </footer>
+    
+
     </div>
   )
 }
